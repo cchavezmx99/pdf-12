c08279464e4331906e21d23fbc4dad64da09c5e5
cchavezmx99 <carlos.chavez@99minutos.con>
2023-02-26 04:01:41 -0600

    probando cors functions

--- pages/api/pdfgenerate.js
+++ pages/api/pdfgenerate.js
@@ -1,5 +1,22 @@
 import pdfGenerator from '../../utils/html2pdf.js'
-export default async function handledr (req, res) {  
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+
+async function handledr (req, res) {  
     
   const { containers, country, station } = req.body  
   try {
@@ -30,3 +47,6 @@ export default async function handledr (req, res) {
   }   
 
 }
+
+
+export default allowCors(handledr)
