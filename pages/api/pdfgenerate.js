d1e4ce677c6753df33547404e321c3e175c08d5d
cchavezmx99 <carlos.chavez@99minutos.con>
2023-02-26 22:17:15 -0600

    no funciona la idea de cors

--- pages/api/pdfgenerate.js
+++ pages/api/pdfgenerate.js
@@ -1,32 +1,18 @@
 import pdfGenerator from '../../utils/html2pdf.js'
-const witheList = process.env.WHITE_LISTED_DOMAINS.split(',') || []
 
 const allowCors = fn => async (req, res) => {
-
-  console.log("🚀 ~ file: pdfgenerate.js:22 ~ allowCors ~ req.headers.origin", req.headers.origin)
-  let isValid = null
-  witheList.forEach(origin => {    
-    isValid = req.headers.origin
-      .split('-')
-      .some(word => origin.includes(word))
-  })
-
-  if (isValid) {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  } 
-
-  return res.status(403).end('Forbidden')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
 }
 
 
@@ -49,8 +35,7 @@ async function handledr (req, res) {
         throw new Error('Invalid station')
       }
 
-      const pdf = await pdfGenerator(containers, country, station)        
-      console.log("🚀 ~ file: pdfgenerate.js:22 ~ handledr ~ pdf:", pdf)
+      const pdf = await pdfGenerator(containers, country, station)
       return res.status(200).json({ pdfUrl: pdf })
     }
 
