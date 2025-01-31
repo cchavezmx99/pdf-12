851cb7b65ea80d668e8b000765b716f47545a99a
cchavezmx99 <carlos.chavez@99minutos.com>
2025-01-31 14:35:39 -0600

    probando chrome spaz

--- utils/html2pdf.js
+++ utils/html2pdf.js
@@ -1,19 +1,54 @@
 import qr from "qrcode";
-import * as puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
+import chromium from "chrome-aws-lambda";
 
 console.log(chromium);
 
 const html2pdf = async (containers, country, station) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: chromium.executablePath(),
-    headless: chromium.headless === "true",
+  const executablePath = await chromium.executablePath;
+  const browser = await chromium.puppeteer.launch({
+    args: [
+      ...chromium.args,
+      "--autoplay-policy=user-gesture-required",
+      "--disable-background-networking",
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-breakpad",
+      "--disable-client-side-phishing-detection",
+      "--disable-component-update",
+      "--disable-default-apps",
+      "--disable-dev-shm-usage",
+      "--disable-domain-reliability",
+      "--disable-extensions",
+      "--disable-features=AudioServiceOutOfProcess",
+      "--disable-hang-monitor",
+      "--disable-ipc-flooding-protection",
+      "--disable-notifications",
+      "--disable-offer-store-unmasked-wallet-cards",
+      "--disable-popup-blocking",
+      "--disable-print-preview",
+      "--disable-prompt-on-repost",
+      "--disable-renderer-backgrounding",
+      "--disable-setuid-sandbox",
+      "--disable-speech-api",
+      "--disable-sync",
+      "--hide-scrollbars",
+      "--ignore-gpu-blacklist",
+      "--metrics-recording-only",
+      "--mute-audio",
+      "--no-default-browser-check",
+      "--no-first-run",
+      "--no-pings",
+      "--no-sandbox",
+      "--no-zygote",
+      "--password-store=basic",
+      "--use-gl=swiftshader",
+      "--use-mock-keychain",
+    ],
+    executablePath,
+    headless: true,
   });
 
   const page = await browser.newPage();
-
   const qrMaker = async (counter) => {
     const qrCode = qr.toString(counter, {
       type: "svg",
